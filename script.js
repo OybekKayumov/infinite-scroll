88996a280a48ba6e64ce0db9b8e096452b4123c9
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-02-25 16:09:05 +0500

    Add fetch

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
 // dotenv
+// require("dotenv").config();
+// import dotenv from 'dotenv'
+// dotenv.config()
+// console.log(process.env.API_KEY)
 
 const imgContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
 let photosArray = [];
 
+function setAttributes(elem, attributes) {
+  for (const key in attributes) {
+    elem.setAttribute(key, attributes[key]);
+  }
+}
+
 function displayPhotos() {
   photosArray.forEach((photo) => {
     const item = document.createElement('a');
-    item.setAttribute('href', photo.links.html);
-    item.setAttribute('target', '_blank');
+    // item.setAttribute('href', photo.links.html);
+    // item.setAttribute('target', '_blank');
+    setAttributes(item, {
+      href: photo.links.html,
+      target: '_blank',
+    })
 
     const img = document.createElement('img');
-    img.setAttribute('src', photo.urls.regular);
-    img.setAttribute('alt', photo.alt_description)
-    img.setAttribute('title', photo.alt_description)
+    // img.setAttribute('src', photo.urls.regular);
+    // img.setAttribute('alt', photo.alt_description)
+    // img.setAttribute('title', photo.alt_description)
+
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    })
 
     item.appendChild(img);
     imgContainer.appendChild(item);
@@ -22,7 +42,8 @@ function displayPhotos() {
 }
 
 const count = 10;
-const apiKey = 'api-key';
+// const apiKey = process.env.API_KEY;
+const apiKey = '';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
 
 async function getPhotos() {
@@ -36,6 +57,8 @@ async function getPhotos() {
     console.log('oops...: ', error);
   }
 }
+
+
 
 
 getPhotos();
